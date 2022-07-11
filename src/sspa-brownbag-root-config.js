2261ed61c16877500b81d398380334aa203ef352
Jean Flores <jeansfloresof@gmail.com>
2022-07-10 21:26:18 -0300

    add new modules

--- src/sspa-brownbag-root-config.js
+++ src/sspa-brownbag-root-config.js
@@ -4,17 +4,25 @@ import {
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
+
 import microfrontendLayout from "./microfrontend-layout.html";
 
 const routes = constructRoutes(microfrontendLayout);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
+
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
+  active: false,
+});
 
 applications.forEach(registerApplication);
+
 layoutEngine.activate();
 start();
